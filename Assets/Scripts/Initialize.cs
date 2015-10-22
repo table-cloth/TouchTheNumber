470232e036f20379b5f8c071c8d3f0f2910fedee
Minami <table.cloth.03@gmail.com>
2015-10-22 23:58:41 +0900

    エディタをmonodevelopに・タップ判定処理および起動時にデバッグ通知を流す処理を実装テスト#2

---
+++ Assets/Scripts/Initialize.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Initialize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Test log
+		Debug.Log ("Start Project: Hello World");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/NumberObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D collision2D = Physics2D.OverlapPoint(tapPoint);
+			if(collision2D) {
+				RaycastHit2D hitObject = Physics2D.Raycast(tapPoint, -Vector2.up);
+				if(hitObject) {
+					Debug.Log("hit object is " + hitObject.collider.gameObject.name);
+				}
+			}
+		}
+	}
+}
